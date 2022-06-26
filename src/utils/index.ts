8ec957f4037ba0f0fbc32f330c0e14e755011c56
Faris Aziz <farisaziz12@gmail.com>
2022-06-26 20:06:10 +0200

    feat(utils): create time, capitalize and unique funcs

---
+++ src/utils/index.ts
@@ -0,0 +1,39 @@
+export const capitalize = (string: string): string => {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+export const timeSince = (date: string): string =>  {
+    const timeFormat = new Intl.RelativeTimeFormat('en', { style: 'long' });
+
+    const now = new Date();
+    const targetDate = new Date(date);
+    const difference = now.getTime() - targetDate.getTime();
+    const millisecondsToDays: number = 1000 * 3600 * 24;
+    const totalDays = Math.ceil(difference / millisecondsToDays);
+
+    // Handling only days, months & years for now
+    if (totalDays === 365) {
+        return timeFormat.format(-(totalDays / 365).toFixed(0), 'year');
+    }
+    if (totalDays > 365) {
+        return timeFormat.format(-(totalDays / 365).toFixed(0), 'years');
+    }
+    if (totalDays < 30) {
+        return timeFormat.format(-(totalDays), 'days');
+    }
+    if (totalDays === 30) {
+        return timeFormat.format(-(totalDays / 30).toFixed(0), 'month');
+    }
+    if (totalDays > 30) {
+        return timeFormat.format(-(totalDays / 30).toFixed(0), 'months');
+    }
+    if (totalDays === 1) {
+        return timeFormat.format(-1, 'day');
+    }
+
+    return ""
+}
+
+export const  unique = (value: any, index: number, self: string | any[]): boolean => {
+  return self.indexOf(value) === index;
+}
