0d381d47dfe8bbc2dd94371b479798007da131c6
Faris Aziz <farisaziz12@gmail.com>
2022-06-26 22:19:34 +0200

    test(utils): add more data test cases

--- src/utils/index.test.ts
+++ src/utils/index.test.ts
@@ -7,8 +7,36 @@ describe("utils", () => {
       expect(typeof timeSince("2021-07-03T22:11:00.000Z")).toBe("string");
     });
 
-    it("should return a string with the correct format", () => {
+    it("should return a string with the correct format for 1 day ago", () => {
+      expect(timeSince("2021-08-02T22:11:00.000Z")).toBe("1 day ago");
+    });
+
+    it("should return a string with the correct format for 2 days ago", () => {
+      expect(timeSince("2021-08-01T22:11:00.000Z")).toBe("2 days ago");
+    });
+
+    it("should return a string with the correct format for 1 week ago", () => {
+      expect(timeSince("2021-07-27T22:11:00.000Z")).toBe("1 week ago");
+    });
+
+    it("should return a string with the correct format for 2 weeks ago", () => {
+      expect(timeSince("2021-07-20T22:11:00.000Z")).toBe("2 weeks ago");
+    });
+
+    it("should return a string with the correct format for 1 month ago", () => {
       expect(timeSince("2021-07-03T22:11:00.000Z")).toBe("1 month ago");
+    });
+
+    it("should return a string with the correct format for 2 months ago", () => {
+      expect(timeSince("2021-06-03T22:11:00.000Z")).toBe("2 months ago");
+    });
+
+    it("should return a string with the correct format for 1 year ago", () => {
+      expect(timeSince("2020-07-27T22:11:00.000Z")).toBe("1 year ago");
+    });
+
+    it("should return a string with the correct format for 2 years ago", () => {
+      expect(timeSince("2019-07-27T22:11:00.000Z")).toBe("2 years ago");
     });
 
     it("should return an empty string when given a bad date", () => {
