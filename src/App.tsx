d1c290dd570f7a88dc3a7bb2636ea8584d78ae78
Faris Aziz <farisaziz12@gmail.com>
2022-06-26 20:06:18 +0200

    feat(app): add filters

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,126 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import { MediaCard } from "./components";
+import { unique } from "./utils";
+import { Media } from "./types";
+import { fetchMedia } from "./api";
+
+const CardsContainer = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+`;
+
+const Title = styled.h1`
+  text-align: center;
+`;
+
+const Select = styled.select`
+  width: 100px;
+`;
+
+const FilterContainer = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  margin-bottom: 20px;
+
+  h2 {
+    text-align: center;
+    font-size: 20px;
+  }
+`;
+
+const handleFilterChange = (
+  event: React.ChangeEvent<HTMLSelectElement>,
+  callback: (value: string) => void
+) => {
+  const { value } = event.target;
+
+  callback(value);
+};
 
 function App() {
+  const [media, setMedia] = useState<Media[]>([]);
+  const [statusFilter, setStatusFilter] = useState<Media["status"] | null>(null);
+  const [langFilter, setLangFilter] = useState<string | null>(null);
+
+  // fetch media from API on component mount
+  useEffect(() => {
+    fetchMedia()
+      .then((mediaData) => setMedia(mediaData))
+      .catch((error) => console.log(error));
+  }, []);
+
+  const statusFilterOptions = [
+    "all",
+    ...media.map(({ status }) => status).filter(unique),
+  ];
+  const languageFilterOptions = [
+    "all",
+    ...media.flatMap(({ languages }) => languages).filter(unique),
+  ];
+
+  const filteredMedia =
+    statusFilter || langFilter
+      ? media.filter(({ status, languages }) => {
+          const statusMatch = statusFilter === null || status === statusFilter;
+          const langMatch = langFilter === null || languages.includes(langFilter);
+
+          // combine filters
+          return statusMatch && langMatch;
+        })
+      : media;
+
+    console.log(filteredMedia);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Title>Subly</Title>
+      <FilterContainer>
+        <div>
+          <h2>Status</h2>
+          <Select
+            onChange={(event) => {
+              if (event.target.value === "all") {
+                setStatusFilter(null);
+                return;
+              }
+              handleFilterChange(event, (value) =>
+                setStatusFilter(value as Media["status"])
+              );
+            }}
+          >
+            {statusFilterOptions.map((status) => (
+              <option key={status} value={status}>
+                {status}
+              </option>
+            ))}
+          </Select>
+        </div>
+        <div>
+          <h2>Language</h2>
+          <Select
+            onChange={(event) => {
+              if (event.target.value === "all") {
+                setLangFilter(null);
+                return;
+              }
+              handleFilterChange(event, (value) => setLangFilter(value as string));
+            }}
+          >
+            {languageFilterOptions.map((status) => (
+              <option key={status} value={status}>
+                {status}
+              </option>
+            ))}
+          </Select>
+        </div>
+      </FilterContainer>
+      <CardsContainer>
+        {filteredMedia.map((data) => (
+          <MediaCard key={data.id} {...data} />
+        ))}
+      </CardsContainer>
+    </>
   );
 }
 
